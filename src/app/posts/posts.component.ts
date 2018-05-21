7737daa2d0fcf108da4246b03887dfc16ad08603
john <odeyjohn4u@gmail.com>
2018-05-21 16:55:47 +0100

    updated json

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -10,15 +10,15 @@ export class PostsComponent implements OnInit {
   posts: any[];
   staffs: any[];
   constructor(http :Http) {
-    http.get('http://jsonplaceholder.typicode.com/posts').
-    subscribe(response=>{
-      this.posts= response.json();
-    });
-
-    // http.get('http://localhost/json/webcoupersData.json')
-    // .subscribe(response=>{
-    //   this.staffs=response.json();
+    // http.get('http://jsonplaceholder.typicode.com/posts').
+    // subscribe(response=>{
+    //   this.posts= response.json();
     // });
+
+    http.get('./assets/data/webcoupersData.json')
+    .subscribe(response=>{
+      this.staffs=response.json();
+    });
    }
 
   ngOnInit() {
