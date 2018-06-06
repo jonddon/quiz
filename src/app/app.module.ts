85d9bd3f6cfca87019b1cbe39a67f2bda4eefcd5
john <odeyjohn4u@gmail.com>
2018-06-06 10:36:34 +0100

    Routing updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { HistoryComponent } from './history/history.component';
-import { RuleComponent } from './rule/rule.component';
 import { HowToPlayComponent } from './how-to-play/how-to-play.component';
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 import { QuizzComponent } from './quizz/quizz.component';
@@ -27,7 +26,6 @@ import { LandingPage2Component } from './landing-page2/landing-page2.component';
     NavbarComponent,
     LandingPageComponent,
     HistoryComponent,
-    RuleComponent,
     HowToPlayComponent,
     LeaderboardComponent,
     QuizzComponent,
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,7 +3,6 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { HistoryComponent } from './history/history.component';
-import { RuleComponent } from './rule/rule.component';
 import { HowToPlayComponent } from './how-to-play/how-to-play.component';
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 import { QuizzComponent } from './quizz/quizz.component';
@@ -15,7 +14,6 @@ export const appRoutes : Routes=[
     {path:'register', component: RegisterComponent},
     {path:'quizz', component: QuizzComponent},
     {path:'history', component: HistoryComponent},
-    {path:'rule', component: RuleComponent},
     {path:'howto', component: HowToPlayComponent},
     {path:'leaderboard', component: LeaderboardComponent}
     
